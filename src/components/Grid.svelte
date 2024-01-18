e5f8a6066c6e642bd81a2eff3c9d0db70b4a981d
EkemRoht <ekemroht@gmail.com>
2024-01-19 03:32:50 +0500

    Grid Generation - Done

--- src/components/Grid.svelte
+++ src/components/Grid.svelte
@@ -1,31 +1,30 @@
 <script>
     import { Stage, Layer, Line, Rect } from 'svelte-konva';
-    import { onMount } from 'svelte';
 
     let width = window.innerWidth;
     let height = window.innerHeight;
     let gridSize = 50;
-    let gridWidth = 10;
-    let gridHeight = 10;
+    let cellCountWidth = 10; // Ограничение по количеству ячеек в ширину
+    let cellCountHeight = 10; // Ограничение по количеству ячеек в высоту
     let lines = generateGrid();
 
     function generateGrid() {
         const lines = [];
         const dashLength = 1; // Расчет длины штрихов
 
-        for (let i = 0; i <= gridWidth * gridSize; i += gridSize) {
+        for (let i = 0; i <= cellCountWidth; i++) {
             lines.push({
-                points: [i + 0.5, 0, i + 0.5, height],
+                points: [i * gridSize + 0.5, 0, i * gridSize + 0.5, cellCountHeight * gridSize],
                 strokeWidth: 1,
-                dash: [dashLength, dashLength*3],
+                dash: [dashLength, dashLength * 3],
             });
         }
 
-        for (let j = 0; j <= gridHeight * gridSize; j += gridSize) {
+        for (let j = 0; j <= cellCountHeight; j++) {
             lines.push({
-                points: [0, j + 0.5, width, j + 0.5],
+                points: [0, j * gridSize + 0.5, cellCountWidth * gridSize, j * gridSize + 0.5],
                 strokeWidth: 1,
-                dash: [dashLength, dashLength*3],
+                dash: [dashLength, dashLength * 3],
             });
         }
 
@@ -35,11 +34,11 @@
     function generateCrosses() {
         const crosses = [];
 
-        for (let i = 0; i < width; i += gridSize) {
-            for (let j = 0; j < height; j += gridSize) {
+        for (let i = 0; i <= cellCountWidth; i++) {
+            for (let j = 0; j <= cellCountHeight; j++) {
                 crosses.push({
-                    x: i + 0.5,
-                    y: j + 0.5,
+                    x: i * gridSize + 0.5,
+                    y: j * gridSize + 0.5,
                 });
             }
         }
